1bb5f49e2ff0271d1a0e00884fcf8b7ae8ece5ae
Steven Egas <sdegas1@espe.edu.ec>
2025-06-29 12:45:15 -0500

    Delete PRY_LOCALSTORAGE directory

--- PRY_LOCALSTORAGE/public/js/guardar_usuario.js
+++
@@ -1,9 +0,0 @@
-
-function guardarUsuario() {
-    // Capturar el valor del campo de texto
-    var nombre_usuario = document.getElementById('txt_nombre_usuario').value;
-    console.log(nombre_usuario)
-    
-    localStorage.setItem('nombre_user', nombre_usuario);
-    alert('Usuario guardado: ' + nombre_usuario);
-}
